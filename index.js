128448a99bee79b4697aac184634b9d193fbf8fd
Michelle Lam <shelly2.lam@gmail.com>
2019-08-05 16:35:15 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function capitalize(str) {
+  return str.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+};
+
 const titleCased = () => {
-  return tutorials
+ return tutorials.map(function(tutorial) {
+  return capitalize(tutorial) }
+  )
 }
+
+
+
+
+
+
+// const titleCased = tutorials.map(function(tutorial) {
+//   let words = tutorial.split(" ")
+//   for (let i = 0; i < words.length; i++) {
+//    return words[i].charAt(0).toUpperCase() + words[i].slice(1)
+//   }
+//  }
+//  )
+ 
