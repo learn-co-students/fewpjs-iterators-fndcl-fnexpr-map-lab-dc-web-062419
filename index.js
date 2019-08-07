72104cae2b3405ef8801633d944fcdad326613e0
AsheToll <github email address>
2019-08-06 21:59:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// // for and for...of
+// let students = ["harry", "ron", "hermione", "ginevra"];
+// let rollCall = [];
+
+// for (const student of students) {
+//   rollCall.push(student + " the wizard" );
+// }
+
+// // map() with a function declaration
+// function studentRollCall(student) {
+//   return student + " the wizard";
+// }
+
+// let rollCall = students.map(studentRollCall);
+
+// // map() with a function expression ("anonymous function")
+// let rollCall = students.map(function(student) {
+//   return student + " the wizard";
+// });
+
+// // map() with an arrow function
+// let rollCall = students.map(student => student + " the wizard")
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
@@ -11,6 +35,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() {
+  return tutorials.map(function(str) {
+    str = str.split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    })
+    return str.join(' ');
+  })
+};
